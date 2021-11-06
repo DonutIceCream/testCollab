aaf79843e222c97540f490bca32da37a70f2da88
Anggi Wahyu Desando <desandowahyu@gmail.com>
2021-11-06 23:23:03 +0700

    Print Hello Alterra

---
+++ src/User.java
@@ -0,0 +1,20 @@
+public class User {
+    String HP;
+    String user;
+
+    public String getHP() {
+        return HP;
+    }
+
+    public void setHP(String HP) {
+        this.HP = HP;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
